31e9323b785695021ba2c4e403193f23426e3594
unknown <A559998@L81046.fr01.awl.atosorigin.net>
2014-07-12 12:23:19 +0200

    Working on new version of cache. Now based on DiskLruCache from Jake Wharton for the disk cache.

--- app/src/main/java/vb/android/library/cache/app/MainActivity.java
+++ app/src/main/java/vb/android/library/cache/app/MainActivity.java
@@ -4,23 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.Date;
-
-import vb.android.library.cache.lib.SimpleCache;
-import vb.android.library.cache.lib.CacheWrapper;
 import vb.android.library.cache.lib.VBLibCacheContextUtils;
 import vb.android.library.cache.lib.VBLibCacheLogUtils;
 
 
 public class MainActivity extends Activity {
 
-    private Button mButtonAddObject10Sec;
-    private Button mButtonAddObjectInfinite;
-    private Button mButtonDisplayObject10Sec;
-    private Button mButtonDisplayObjectInfinite;
-    private Button mButtonClearCache;
+    private Button mButtonAddObjectA;
+    private Button mButtonAddObjectB;
+    private Button mButtonDisplayObjectB;
+    private Button mButtonDisplayObjectA;
+    private Button mButtonInvalidateCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,67 +25,45 @@ public class MainActivity extends Activity {
         VBLibCacheLogUtils.enableLog();
         VBLibCacheContextUtils.setContext(getApplicationContext());
 
-        mButtonAddObject10Sec = (Button) findViewById(R.id.buttonAddObject10ToCache);
-        mButtonAddObjectInfinite = (Button) findViewById(R.id.buttonAddObjectInfiniteLifetime);
-        mButtonClearCache = (Button) findViewById(R.id.buttonClearCache);
-        mButtonDisplayObjectInfinite = (Button) findViewById(R.id.buttonDisplayObjectInfiniteLifetime);
-        mButtonDisplayObject10Sec = (Button) findViewById(R.id.buttonDisplayObject10Sec);
+        mButtonAddObjectA = (Button) findViewById(R.id.buttonAddObjectAToCache);
+        mButtonAddObjectB = (Button) findViewById(R.id.buttonAddObjectBToCache);
+        mButtonInvalidateCache = (Button) findViewById(R.id.buttonInvalidateCache);
+        mButtonDisplayObjectA = (Button) findViewById(R.id.buttonDisplayObjectB);
+        mButtonDisplayObjectB = (Button) findViewById(R.id.buttonDisplayObjectA);
 
-        mButtonAddObject10Sec.setOnClickListener(new View.OnClickListener() {
+        mButtonAddObjectA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Date date = new Date();
-                date.setTime(date.getTime() + 10000);
 
-                // String is serializable so can be cached.
-                CacheWrapper wrapper = new CacheWrapper("object10sec", date);
-                SimpleCache cache = SimpleCache.getCacheManager("mycache");
-                cache.put("object10sec", wrapper);
             }
         });
 
-        mButtonAddObjectInfinite.setOnClickListener(new View.OnClickListener() {
+        mButtonAddObjectB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                // String is serializable so can be cached.
-                CacheWrapper wrapper = new CacheWrapper("objectInfinite", null);
-                SimpleCache cache = SimpleCache.getCacheManager("mycache");
-                cache.put("objectInfinite", wrapper);
+
             }
         });
 
-        mButtonDisplayObject10Sec.setOnClickListener(new View.OnClickListener() {
+        mButtonDisplayObjectB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SimpleCache cache = SimpleCache.getCacheManager("mycache");
-                String object = null;
-                object = cache.get("object10sec", String.class);
-                if (object != null) {
-                    Toast.makeText(MainActivity.this, object, Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(MainActivity.this, "object10sec is not in cache", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        mButtonDisplayObjectInfinite.setOnClickListener(new View.OnClickListener() {
+        mButtonDisplayObjectA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SimpleCache cache = SimpleCache.getCacheManager("mycache");
-                String object = null;
-                object = cache.get("objectInfinite", String.class);
-                if (object != null) {
-                    Toast.makeText(MainActivity.this, object, Toast.LENGTH_SHORT).show();
-                } else
-                    Toast.makeText(MainActivity.this, "objectInfinite is not in cache", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        mButtonClearCache.setOnClickListener(new View.OnClickListener() {
+        mButtonInvalidateCache.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SimpleCache cache = SimpleCache.getCacheManager("mycache");
-                cache.invalidate();
+                
             }
         });
 
---
+++ lib/src/main/java/vb/android/library/cache/lib/DualCache.java
@@ -0,0 +1,131 @@
+package vb.android.library.cache.lib;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+/**
+ * Created by Vincent Brison.
+ */
+public class DualCache<T> {
+
+    private static String CACHE_FILE_PREFIX = "dualcache";
+
+    private StringCacheLru mRamCacheLru;
+    private DiskLruCache mDiskLruCache;
+    private Class<T> mClazz;
+
+    private String mId;
+
+    private static ObjectMapper mMapper = new ObjectMapper();
+
+    public DualCache(String id, int appVersion, int ramCacheSizeInBytes, int diskCacheSizeInBytes, Class<T> clazz) {
+        mRamCacheLru = new StringCacheLru(ramCacheSizeInBytes);
+        mId = id;
+        mClazz = clazz;
+        File folder = new File(VBLibCacheContextUtils.getContext().getCacheDir().getPath() + "/" + CACHE_FILE_PREFIX + "/" + mId);
+        try {
+            mDiskLruCache = DiskLruCache.open(folder, appVersion, 1, diskCacheSizeInBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void put(String key, T object) {
+        VBLibCacheLogUtils.logInfo("Object " + key + " is saved in cache.");
+        String stringObject = null;
+        try {
+            stringObject = mMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        mRamCacheLru.put(key, stringObject);
+        try {
+            DiskLruCache.Editor editor = mDiskLruCache.edit(getDiskFileNameFromKey(key));
+            editor.set(0, stringObject);
+            editor.commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public T get(String key) {
+
+        String stringObject;
+        DiskLruCache.Snapshot snapshotObject = null;
+
+        // Try to get the object from RAM.
+        stringObject = mRamCacheLru.get(key);
+
+        if (stringObject == null) {
+            // Try to get the cached object from disk.
+            VBLibCacheLogUtils
+                    .logInfo("Object " + key + " is not in the RAM. Try to get it from disk.");
+            try {
+                snapshotObject = mDiskLruCache.get(getDiskFileNameFromKey(key));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (snapshotObject != null) {
+                VBLibCacheLogUtils.logInfo("Object " + key + " is on disk.");
+
+                // Refresh object in ram.
+                try {
+                    mRamCacheLru.put(key, snapshotObject.getString(0));
+
+                    //JavaType type = mMapper.getTypeFactory().constructType(Type)
+                    return mMapper.readValue(snapshotObject.getString(0), mClazz);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } else
+                VBLibCacheLogUtils.logInfo("Object " + key + " is not on disk.");
+
+        } else {
+            VBLibCacheLogUtils.logInfo("Object " + key + " is in the RAM.");
+            try {
+                return mMapper.readValue(stringObject, mClazz);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        // No data are available.
+        return null;
+    }
+
+    public void invalidate() {
+        try {
+            mDiskLruCache.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mRamCacheLru.evictAll();
+    }
+
+    private String getDiskFileNameFromKey(String key) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(key.getBytes());
+            byte byteData[] = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte aByteData : byteData) {
+                sb.append(Integer.toString((aByteData & 0xff) + 0x100, 16).substring(1));
+            }
+            return CACHE_FILE_PREFIX + "-" + mId + "-" + sb;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
